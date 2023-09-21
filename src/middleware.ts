64d14e7a657adaa0d80dc4fafd173a7c5591bd39
Tarun <tarunpahade6969@gmail.com>
2023-09-22 00:44:28 +0530

    Removing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,27 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
 
-  const path=request.nextUrl.pathname
-  const isPublicPath=path === '/' || path === '/login' || path === '/signup' || path === '/reset' 
- const token= request.cookies.get('next-auth.session-token')?.value || ''
-const tokenPath=['/profile','/teacher','/student']
+  //const path=request.nextUrl.pathname
+//  const isPublicPath=path === '/' || path === '/login' || path === '/signup' || path === '/reset' 
+// const token= request.cookies.get('next-auth.session-token')?.value || ''
+// const tokenPath=['/profile','/teacher','/student']
 
- if (isPublicPath && token) {
-   return NextResponse.redirect(new URL('/student',request.nextUrl))
- }
- if (isPublicPath && token) {
-  return NextResponse.redirect(new URL('/teacher',request.nextUrl))
-}
+//  if (isPublicPath && token) {
+//    return NextResponse.redirect(new URL('/student',request.nextUrl))
+//  }
+//  if (isPublicPath && token) {
+//   return NextResponse.redirect(new URL('/teacher',request.nextUrl))
+// }
 
-if (isPublicPath && token) {
-  return NextResponse.redirect(new URL('/profile',request.nextUrl))
-}
- if(!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/login',request.nextUrl))
-  }
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/assignment',request.nextUrl))
-  }
+// if (isPublicPath && token) {
+//   return NextResponse.redirect(new URL('/profile',request.nextUrl))
+// }
+//  if(!isPublicPath && !token){
+//     return NextResponse.redirect(new URL('/login',request.nextUrl))
+//   }
+//   if(isPublicPath && token){
+//     return NextResponse.redirect(new URL('/assignment',request.nextUrl))
+//   }
 
 }
  
